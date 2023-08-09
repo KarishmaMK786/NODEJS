58ea1496b5d2b939f179c71f928877571850ba85
KarishmaMK <matazkk786@gmail.com>
2023-08-09 10:11:03 +0530

    files are added

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name : "Kempi", email: 'kempi@786'},
+    {name : "Kempi", email: 'kempi@786'},
+    {name : "Kempi", email: 'kempi@786'}
+]
+
+module.exports = data;
---
+++ sample.js
@@ -0,0 +1,25 @@
+// const app = require('./app.js');
+// const fs = require('fs');
+
+// console.log(app);
+// console.log("Kempi");
+
+// fs.writeFileSync("Hello.txt", "Kempi");
+
+// console.log (__dirname);
+// console.log (__filename);
+
+// const http = require("http");
+
+// http.createServer((req, resp) => {
+//     resp.write("<h1>Hello Kempi</h1>");
+//     resp.end();
+// }).listen(4500);
+
+const colors = require ("colors");
+
+console.log("Hii Kempi Its Me ".blue);
+
+
+const chalk = require("chalk");
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
